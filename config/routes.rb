08d299354dd5c0d0452bfd08f36d614bdba5f2f1
trevornelson <trevor.nelson1@gmail.com>
2015-04-10 08:43:21 -0400

    Created initial routes for root and login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'competitors#index'
+  get 'login' => 'sessions#new'
+  post 'sessions' => 'sessions#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
