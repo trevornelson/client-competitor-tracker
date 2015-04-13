f6da739b16a9ad71c619dbebdb5cd46effae35a8
trevornelson <trevor.nelson1@gmail.com>
2015-04-13 13:15:00 -0400

    Created spec expectations for Competitor model

---
+++ spec/models/competitor_model_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe Competitor do
+  describe "#fetch_clients method" do
+    
+    it "should return an ActiveRecord collection of client objects" do
+
+    end
+
+    it "should create a new client object for new clients in HTML" do
+
+    end
+
+    it "should update a client record when a client is removed from HTML" do
+
+    end
+
+  end
+end
