3a2043f4ef4917d395124c54a09b1a1f53a04770
trevornelson <trevor.nelson1@gmail.com>
2015-04-13 16:28:42 -0400

    Created fetch_clients supporting private methods

--- app/models/competitor.rb
+++ app/models/competitor.rb
@@ -1,8 +1,40 @@
+require 'open-uri'
+
 class Competitor < ActiveRecord::Base
 	belongs_to :user
 	has_many :clients
 
   def fetch_clients
+    scrape
     clients
+  end
+
+  private
+
+  def scrape
+    page_content = Nokogiri::HTML(open(client_url))
+    parse(page_content)
+  end
+
+  def parse(page_content)
+    nodes = page_content.xpath(client_xpath)
+    if nodes
+      parsed_clients = nodes.map { |node| node.value }
+      update_clients(parsed_clients)
+    end
+  end
+
+  def update_clients(parsed_clients)
+    clients.each do |existing_client|
+      unless parsed_clients.include?(existing_client.name)
+        existing_client.update(dropped: true)
+      end
+    end
+
+    parsed_clients.each do |parsed_client|
+      unless clients.find_by(name: parsed_client)
+        clients.create(name: parsed_client)
+      end
+    end
   end
 end
