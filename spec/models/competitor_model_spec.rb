c7e93251b4ec6cde187cd23e314f5f22dc48ee4e
trevornelson <trevor.nelson1@gmail.com>
2015-04-13 13:28:44 -0400

    Disabled external requests from competitor model spec

--- spec/models/competitor_model_spec.rb
+++ spec/models/competitor_model_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
+require 'webmock/rspec'
+WebMock.disable_net_connect!(allow_localhost: true)
 
 describe Competitor do
   describe "#fetch_clients method" do
     
+
     it "should return an ActiveRecord collection of client objects" do
 
     end
