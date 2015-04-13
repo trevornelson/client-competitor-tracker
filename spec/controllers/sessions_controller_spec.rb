be00750f43745471974bdf506296e9cb85df4f57
trevornelson <trevor.nelson1@gmail.com>
2015-04-13 12:40:17 -0400

    Updated sessions controller spec to follow rails-rspec conventions

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -11,16 +11,17 @@ describe SessionsController do
 	end
 
 	describe "#create controller action" do
-		let(:user) {User.new(email: 'test@example.com', password: '12345')}
-
+		
 		it "should add user_id to session if credentials are correct" do
-			post :new_session, {email: 'test@example.com', password: '12345'}
-			expect(session[:user_id]).to be_nil
+			post :create, {email: 'testinguser@example.com', password: '12345'}
+			expect(response.code).to eq("302")
+			expect(response).to redirect_to(root_path)
 		end
 
 		it "should redirect back to login page if credentials are not correct" do
-			post :new_session, {email: 'test@example.com', password: 'wrong'}
-			expect(session[:user_id]).to be
+			post :create, {email: 'testinguser@example.com', password: 'wrong'}
+			expect(response.code).to eq("302")
+			expect(response).to redirect_to(login_path)
 		end
 	end
 end
