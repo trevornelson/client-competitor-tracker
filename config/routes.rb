b3909ab302e39587557313105baf012dba878434
trevornelson <trevor.nelson1@gmail.com>
2015-04-13 18:26:45 -0400

    Added logout route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'competitors#index'
   get 'login' => 'sessions#new'
+  get 'logout' => 'sessions#destroy'
   post 'sessions' => 'sessions#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
