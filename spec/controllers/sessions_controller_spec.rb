953c02759859ef71675fe443abb42217027e39f4
trevornelson <trevor.nelson1@gmail.com>
2015-04-13 14:50:00 -0400

    Fixed sessions controller failed spec due to migration update

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -11,15 +11,18 @@ describe SessionsController do
 	end
 
 	describe "#create controller action" do
+		before(:all) do
+			User.create(email: 'testuser@example.com', password: '12345')
+		end
 		
 		it "should add user_id to session if credentials are correct" do
-			post :create, {email: 'testinguser@example.com', password: '12345'}
+			post :create, {email: 'testuser@example.com', password: '12345'}
 			expect(response.code).to eq("302")
 			expect(response).to redirect_to(root_path)
 		end
 
 		it "should redirect back to login page if credentials are not correct" do
-			post :create, {email: 'testinguser@example.com', password: 'wrong'}
+			post :create, {email: 'testuser@example.com', password: 'wrong'}
 			expect(response.code).to eq("302")
 			expect(response).to redirect_to(login_path)
 		end
