c3256d2a14ff452ad400bed3de9d5aa8168e1181
trevornelson <trevor.nelson1@gmail.com>
2015-04-13 15:22:56 -0400

    Finished initial competitor model tests

--- spec/models/competitor_model_spec.rb
+++ spec/models/competitor_model_spec.rb
@@ -4,38 +4,34 @@ WebMock.disable_net_connect!(allow_localhost: true)
 
 describe Competitor do
   describe "#fetch_clients method" do
-    let(:competitor) { Competitor.create() }
-    let(:initial_request_body) { '<div class="clients">Client 1</div><div class="clients">Client 2</div><div class="clients">Client 3</div>' }
-    let(:second_request_body) { '<div class="clients">Client 1</div><div class="clients">Client 3</div><div class="clients">Client 4</div>' }
+    before(:all) do
+      response_body = '<div class=clients>Client 1</div><div class=clients>Client 2</div><div class=clients>Client 4</div><div class=clients>Client 5</div>'
 
-    it "should return an ActiveRecord collection of client objects" do
       stub_request(:get, "www.example.com/clients").
-        to_return(:body => initial_request_body, :status => 200)
+        to_return(:body => response_body, :status => 200)
 
+      @test_competitor = FactoryGirl.create(:competitor)
+
+      for n in 1..4 do
+        @test_competitor.clients.create(name: "Client #{n}")
+      end
     end
 
-    it "should create new client objects from initial request" do
-      stub_request(:get, "www.example.com/clients").
-        to_return(:body => initial_request_body, :status => 200)
+    it "should return an ActiveRecord collection of client objects" do
 
-
-
+      expect(@test_competitor.fetch_clients.class).to eq('Client::ActiveRecord_Associations_CollectionProxy')
     end
 
     it "should create a new client object for new clients added to page content" do
-      stub_request(:get, "www.example.com/clients").
-        to_return(:body => second_request_body, :status => 200)
-    
-
-
+      @test_competitor.fetch_clients
+      added_client = Client.find_by(name: 'Client 5')
+      expect(added_client).to be_truthy
     end
 
     it "should update a client record when a client is removed from page content" do
-      stub_request(:get, "www.example.com/clients").
-        to_return(:body => second_request_body, :status => 200)
-
-
-
+      @test_competitor.fetch_clients
+      removed_client = Client.find_by(name: 'Client 3')
+      expect(removed_client.dropped).to be_truthy 
     end
 
   end
