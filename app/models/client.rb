5230bbc781f9815ad3e4698c7e8c07de61c416da
trevornelson <trevor.nelson1@gmail.com>
2015-04-13 19:42:57 -0400

    Created past_year Client class methods

--- app/models/client.rb
+++ app/models/client.rb
@@ -5,4 +5,12 @@ class Client < ActiveRecord::Base
   def updated_at_to_s
     updated_at.strftime("%m/%d/%Y")
   end
+
+  def self.updated_in_past_year
+    where("updated_at > ?", Time.now - 365.days).count
+  end
+
+  def self.dropped_in_past_year
+    where("updated_at > ? AND dropped = ?", Time.now - 365.days, true).count
+  end
 end
