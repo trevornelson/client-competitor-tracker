3576737e8633ba0a19e069aef3386d5e4bf77132
trevornelson <trevor.nelson1@gmail.com>
2015-04-13 20:46:50 -0400

    Added register user route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   root 'competitors#index'
+  post 'competitors' => 'competitors#create'
+  
+  post 'scrape' => 'users#scrape'
+  get 'register' => 'users#new'
+  post 'users' => 'users#create'
+
   get 'login' => 'sessions#new'
   get 'logout' => 'sessions#destroy'
   post 'sessions' => 'sessions#create'
-  post 'scrape' => 'users#scrape'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
