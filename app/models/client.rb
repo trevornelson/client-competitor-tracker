e0e6f5c0997051ff8b2cea83251c9aa82dda0667
trevornelson <trevor.nelson1@gmail.com>
2015-04-13 20:01:07 -0400

    Finished client retention rate

--- app/models/client.rb
+++ app/models/client.rb
@@ -6,11 +6,4 @@ class Client < ActiveRecord::Base
     updated_at.strftime("%m/%d/%Y")
   end
 
-  def self.updated_in_past_year
-    where("updated_at > ?", Time.now - 365.days).count
-  end
-
-  def self.dropped_in_past_year
-    where("updated_at > ? AND dropped = ?", Time.now - 365.days, true).count
-  end
 end
--- app/models/competitor.rb
+++ app/models/competitor.rb
@@ -9,7 +9,24 @@ class Competitor < ActiveRecord::Base
     clients
   end
 
+  def annual_retention_rate
+    retention_rate(dropped_in_past(365.days), updated_in_past(365.days))
+  end
+
   private
+
+
+  def updated_in_past(time)
+    clients.where("updated_at > ?", Time.now - time).count
+  end
+
+  def dropped_in_past(time)
+    clients.where("updated_at > ? AND dropped = ?", Time.now - time, true).count
+  end
+
+  def retention_rate(dropped_clients, total_clients)
+    (dropped_clients.to_f / total_clients.to_f)
+  end
 
   def scrape
     page_content = Nokogiri::HTML(open(client_url))
