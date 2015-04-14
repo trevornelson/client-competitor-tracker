d9e935e4fb12d6cb1946af17a61ae5651e0ba176
trevornelson <trevor.nelson1@gmail.com>
2015-04-13 22:25:05 -0400

    Completed new user registration flow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,13 +5,25 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    user = User.create(user_params)
+    if user
+      session[:user_id] = user.id
+      redirect_to root_url
+    else
+      redirect_to :back
+    end
   end
 
   def scrape
     user = User.find_by(session[:user_id])
     user.scrape_competitors
     redirect_to root_url
+  end
+
+  private
+
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
   end
 
 end
