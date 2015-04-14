8f3d39735ab271c93584915893c129afa17fcfc3
trevornelson <trevor.nelson1@gmail.com>
2015-04-13 20:47:21 -0400

    Created users#new controller action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
 
+  def new
+    redirect_to root_url if session[:user_id]
+  end
+
+  def create
+
+  end
+
   def scrape
     user = User.find_by(session[:user_id])
     user.scrape_competitors
