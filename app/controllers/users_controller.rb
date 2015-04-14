f849679fe441a11810d26c3deb5127e238543fe0
trevornelson <trevor.nelson1@gmail.com>
2015-04-14 08:28:27 -0400

    Fixed find_by method bug by adding id attribute

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   end
 
   def scrape
-    user = User.find_by(session[:user_id])
+    user = User.find_by(id: session[:user_id])
     user.scrape_competitors
     redirect_to root_url
   end
