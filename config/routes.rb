c4923232b5d533dab427d8fb1e8c3f0124ac1924
trevornelson <trevor.nelson1@gmail.com>
2015-04-13 20:09:34 -0400

    Created users#scrape route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new'
   get 'logout' => 'sessions#destroy'
   post 'sessions' => 'sessions#create'
+  post 'scrape' => 'users#scrape'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
