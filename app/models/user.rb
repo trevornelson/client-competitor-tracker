4d8c416aef7ce4300c3056ad5336233c6484f7c0
trevornelson <trevor.nelson1@gmail.com>
2015-04-13 20:10:43 -0400

    Added scrape_competitors method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ActiveRecord::Base
 	has_many :competitors
 
 	has_secure_password
+
+  def scrape_competitors
+    competitors.each do |competitor|
+      competitor.fetch_clients
+    end
+  end
 end
